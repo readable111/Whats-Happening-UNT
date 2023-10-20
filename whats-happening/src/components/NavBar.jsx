a834ea36d660d11218a08b4e46b4bd3ef0514c60
Malik G.J. Karunanayake <gamma.gamerz.malik@gmail.com>
2023-10-20 16:55:09 -0500

    updated navbar

--- whats-happening/src/components/NavBar.jsx
+++ whats-happening/src/components/NavBar.jsx
@@ -2,24 +2,22 @@ import React from 'react'
 
 //to add a new element to the NavBar, you can just copy and past a new list element, 
 const NavBar = () => {
-  const liClasses = "block text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent";
   return (
-
-    <nav className="bg-white border-gray-200 dark:bg-gray-900">
+    <nav >
       <div className="hidden w-full md:block md:w-auto" id="navbar-default">
-        <ul className="font-medium flex flex-col p-4 md:p-2 mt-4 border border-gray-100 rounded-lg bg-gray-50 md:flex-row md:space-x-8 md:mt-0 md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
-          <li>
-            <a href="/" className="block text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 md:pl-4 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">Home</a>
-          </li>
-          <li>
-            <a href="/Search" className={liClasses}>Search</a>
-          </li>
-          <li>
-            <a href="/Events" className={liClasses}>Events</a>
-          </li>
+        <ul className="font-medium flex flex-col p-4 md:p-2 border-gray-100 rounded-none bg-gray-50 md:flex-row md:space-x-8 md:mt-0 md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-emerald-950 dark:border-gray-700">
+          <a href="/" className="block md:p-0 md:pl-4">
+            <button class="px-4 py-1 text-white rounded-full shadow-sm hover:bg-green-500/75 ease-in-out delay-150 duration-300">Home</button>
+          </a>
+          <a href="/Search" className="block md:p-0 ">
+            <button class="px-4 py-1 text-white rounded-full shadow-sm hover:bg-sky-500/75 ease-in-out delay-150 duration-300">Search</button>
+          </a>
+          <a href="/Events" className="block md:p-0">
+            <button class="px-4 py-1 text-white rounded-full shadow-sm hover:bg-pink-500/75 ease-in-out delay-150 duration-300">Events</button>
+          </a>
           <li className="md:flex-grow"></li>
           <a href="/Login" className="block md:p-0 md:pr-4">
-            <button class="px-2 py-1 bg-blue-500 text-white rounded-md shadow-sm hover:bg-blue-500/75 ease-in-out delay-150 duration-300">Login</button>
+            <button class="px-4 py-1 bg-teal-600 text-white rounded-full shadow-sm hover:bg-teal-600/75 ease-in-out delay-150 duration-300">Login</button>
           </a>
         </ul>
       </div>
--- whats-happening/src/pages/Search.js
+++ whats-happening/src/pages/Search.js
@@ -1,9 +1,9 @@
 function Search() {
     return (
-        <div className = "font-mono p-2 h-screen bg-white mx-20 flex border-black border">
+        <div className="font-mono p-2 h-screen bg-white mx-20 flex border-black border">
             This is where the Search will go
         </div>
     )
-    }
-    
-    export default Search;
+}
+
+export default Search;
