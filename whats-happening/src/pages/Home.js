e548bc986543287d8a852fb74c9639abaf6571b3
Tyler Bowen <tylerbowen@Tylers-MacBook-Air-6.local>
2023-11-03 11:05:30 -0500

    working on the home page

--- whats-happening/src/pages/Home.js
+++ whats-happening/src/pages/Home.js
@@ -6,9 +6,9 @@ const Home = ({eventData}) => {
         return(
         <div className = "font-mono p-2 h-screen bg-white mx-20 flex border-black border">
             <div className = "w-1/2 flex items-start">
-                <ul className = " h-3/4 p-5 border-black bg-gray-600 w-full overflow-auto"> 
+                <ul className = "h-3/4  border-black bg-gray-200 w-full overflow-auto"> 
                     { eventData.map((item) => {
-                         return <li className="w-fit h-5 border-black">{item.Title}</li>;
+                         return <li className="p-5 w-full h-5 border border-black align-top">{item.Title}</li>;
                      }) }    
                 </ul>
             </div>
