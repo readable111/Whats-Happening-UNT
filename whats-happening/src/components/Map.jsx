54f22bb2561982d239f3773dbc0ae7bc6850b91f
Tyler Bowen <tylerbowen@Tylers-MacBook-Air-6.local>
2023-11-06 17:19:58 -0600

    added the map functionality to the home page

---
+++ whats-happening/src/components/Map.jsx
@@ -0,0 +1,46 @@
+import React from 'react'
+import { GoogleMap, useJsApiLoader } from '@react-google-maps/api'
+
+
+  
+  const center = {
+    lat: 33.2075,
+    lng: -97.1526
+  };
+  
+  
+
+
+function MyMap(location){
+    const {isLoaded} = useJsApiLoader({
+        id: 'google-map-script',
+        googleMapsApiKey: 'AIzaSyD7qBC7GZcM_hVO-H5gcuF9BP-eoA4AsBM'
+    })
+
+    const [map, setMap] = React.useState(null)
+    const onLoad = React.useCallback(function callback(map){
+
+    const bounds = new window.google.maps.LatLngBounds(center);
+    map.fitBounds(bounds);
+
+    setMap(map)
+  }, [])
+
+  const onUnmount = React.useCallback(function callback(map) {
+    setMap(null)
+  }, [])
+
+  return isLoaded ? (
+      <GoogleMap
+        center={center}
+        zoom={10}
+        onLoad={onLoad}
+        onUnmount={onUnmount}
+      >
+        { /* Child components, such as markers, info windows, etc. */ }
+        <></>
+      </GoogleMap>
+  ) : <></>
+}
+
+export default React.memo(MyMap)
