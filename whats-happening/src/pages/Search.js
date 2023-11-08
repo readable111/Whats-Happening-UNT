7b8eb0d7125445ee95eea8150a6847a812298ae2
GivaX <gamma.gamerz.malik@gmail.com>
2023-11-07 18:45:15 -0600

    started implementing the Search functionality

--- whats-happening/src/pages/Search.js
+++ whats-happening/src/pages/Search.js
@@ -1,7 +1,42 @@
+import { useState, useRef } from "react";
+
 function Search() {
+
+    const [userInput, setUserInput] = useState("");
+    const [events, setEvents] = useState([]);
+    const inputRef = useRef();
+
+    const onSubmit = (e) => {
+        e.preventDefault();
+
+        const value = inputRef.current.value;
+        inputRef.current.value = "";
+    }
+
+    const handleUserInputs = (e) => {
+        setUserInput(e.target.value);
+    }
+
+    /*const handleSearchClick = () => {
+        setUserInput("");
+    }*/
+
     return (
         <div className="font-mono p-2 h-screen bg-white mx-20 flex border-black border">
-            This is where the Search will go
+            <div >
+                <form onSubmit={onSubmit}>
+                    <input
+                        type="string"
+                        placeholder="Search for Event"
+                        ref={inputRef}
+                        value={userInput}
+                        onChange={handleUserInputs}
+                    />
+
+                    <button type="submit">Enter</button>
+                </form>
+            </div>
+
         </div>
     )
 }
