20a440cc293c62e4df70f4933a5dcb23b7bc66fe
GivaX <gamma.gamerz.malik@gmail.com>
2023-11-11 01:16:59 -0600

    copied some code from different branch

--- whats-happening/src/pages/Home.js
+++ whats-happening/src/pages/Home.js
@@ -1,9 +1,21 @@
-function Home() {
-    return (
+import React from 'react'
+// import Event from '../components/Events'
+// import Map from '../components/Map'
+
+const Home = ({eventData}) => {
+        return(
         <div className = "font-mono p-2 h-screen bg-white mx-20 flex border-black border">
-            This is where the map and stuff go
+            <div className = "w-1/2 flex items-start">
+                <ul className = "h-3/4  border-black bg-gray-200 w-full overflow-auto"> 
+                    { eventData.map((item) => {
+                         return <li className="p-5 w-full h-5 border border-black align-top">{item.Title}</li>;
+                     }) }    
+                </ul>
+    
+            </div>
+            {/* <Map/> */}
         </div>
-    );
-    }
+        );  
+}
     
     export default Home;
