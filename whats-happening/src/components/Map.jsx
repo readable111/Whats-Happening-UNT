ca24ea88a4a540a549ff34031c103e9f0e74253a
Tyler Bowen <tylerbowen@dynamic-10-125-218-213.frisco.wireless.unt.edu>
2023-11-17 14:31:18 -0600

    updated map functionality

--- whats-happening/src/components/Map.jsx
+++ whats-happening/src/components/Map.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from 'react'
 
-import { GoogleMap, useJsApiLoader, Marker } from '@react-google-maps/api';
+import { GoogleMap, useJsApiLoader,  } from '@react-google-maps/api';
 
 
 //const google = window.google = window.google ? window.google : {}
@@ -14,28 +14,63 @@ const center = {
 
 
 function Maps(location){
-
-
-const { isLoaded } = useJsApiLoader({
-  googleMapsApiKey: process.env.REACT_APP_GOOGLE_MAPS_API_KEY,
+const { isLoaded }  = useJsApiLoader({
+  id: 'google-map-script',
+  //googleMapsApiKey: process.env.REACT_APP_GOOGLE_MAPS_API_KEY,
+  googleMapsApiKey: 'AIzaSyCberKoelaE5uWHmTgo_lxNlMXvU92BrhE',
   libraries: ['places']
 })
 
+const [map, setMap] = React.useState(null);
 
-return(
-  <>
+  const onLoad = React.useCallback(function callback(map) {
+    // This is just an example of getting and using the map instance!!! don't just blindly copy!
+    const bounds = new window.google.maps.LatLngBounds(center);
+    map.fitBounds(bounds);
+
+    setMap(map)
+  }, [])
+
+  const onUnmount = React.useCallback(function callback(map) {
+    const Place = new google.maps.places.PlaceService(map);
+    
+    var request = {
+        query: location,
+        fields:  ['displayName']
+    }
+    Place.findPlaceFromQuery(request, function(results, status) {
+      if (status === google.maps.places.PlacesServiceStatus.OK) {
+        for (var i = 0; i < results.length; i++) {
+          //createMarker(results[i]);
+        }
+        map.setCenter(results[0].geometry.location);
+      }
+    });
+  
+    setMap(null)
+  }, [])
+
+
+return isLoaded ? (
+  
   <GoogleMap 
   center = {center}
-   zoom ={15}
+  zoom ={15}
   mapContainerStyle={{width:'50%', height: '100%' }}
   options={{
       zoomControl: false,
       streetViewControl: false,
       mapTypeControl: false,
-  }}><Marker position={center}/> center=</GoogleMap>
-  </>
-)
+  }}
+  onLoad={onLoad}
+  onUnmount={onUnmount}
+  >
+  <></>
+  </GoogleMap>
+  
+): <></>
 
 }
-export default Maps;
+
+export default React.memo(Maps);
 
