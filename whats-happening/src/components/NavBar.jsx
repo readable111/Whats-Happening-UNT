69082ac6129ae9ad18f39b27dfe15ec22fda97fd
Tyler Bowen <tylerbowen@dynamic-10-125-218-213.frisco.wireless.unt.edu>
2023-11-17 15:11:43 -0600

    added malik's navbar redesign

--- whats-happening/src/components/NavBar.jsx
+++ whats-happening/src/components/NavBar.jsx
@@ -3,23 +3,26 @@ import React from 'react'
 //to add a new element to the NavBar, you can just copy and past a new list element, 
 const NavBar = () => {
   return (
-        <nav className = "bg-white border-gray-200 dark:bg-gray-900">
-          <div className = "hidden w-full md:block md:w-auto" id= "navbar-default">
-          <ul className = "font-medium flex flex-col p-4 md:p-0 mt-4 border border-gray-100 rounded-lg bg-gray-50 md:flex-row md:space-x-8 md:mt-0 md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
-            <li>
-              <a href = "/" className = "block px-3 py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">Home</a>
-            </li>
-            <li>
-              <a href = "/Search" className = "block px-3 py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">Search</a>
-            </li>
-            <li>
-              <a href = "/About" className = "block px-3 py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">About</a>
-            </li>
-          </ul>
-          </div>
-        </nav>
-    
-  )
-}  
+    <nav >
+      <div className="hidden w-full md:block md:w-auto" id="navbar-default">
+        <ul className="font-medium flex flex-col p-4 md:p-2 border-gray-100 rounded-none bg-gray-50 md:flex-row md:space-x-8 md:mt-0 md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-emerald-950 dark:border-gray-700">
+          <a href="/" className="block md:p-0 md:pl-4">
+            <button class="px-4 py-1 text-white rounded-full shadow-sm hover:bg-green-500/75 ease-in-out delay-150 duration-300">Home</button>
+          </a>
+          <a href="/Search" className="block md:p-0 ">
+            <button class="px-4 py-1 text-white rounded-full shadow-sm hover:bg-sky-500/75 ease-in-out delay-150 duration-300">Search</button>
+          </a>
+          <a href="/Events" className="block md:p-0">
+            <button class="px-4 py-1 text-white rounded-full shadow-sm hover:bg-pink-500/75 ease-in-out delay-150 duration-300">Events</button>
+          </a>
+          <li className="md:flex-grow"></li>
+          <a href="/Login" className="block md:p-0 md:pr-4">
+            <button class="px-4 py-1 bg-teal-600 text-white rounded-full shadow-sm hover:bg-teal-600/75 ease-in-out delay-150 duration-300">Login</button>
+          </a>
+        </ul>
+      </div>
+    </nav>
 
+  )
+}
 export default NavBar
